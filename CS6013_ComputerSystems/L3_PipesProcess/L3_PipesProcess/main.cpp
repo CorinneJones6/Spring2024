c8d0791542735dcd7f375c24f64e7907eb292bb7
Corinne Jones <jonescorinne6@gmail.com>
2024-02-05 20:26:56 -0700

    changes

--- CS6013_ComputerSystems/L3_PipesProcess/L3_PipesProcess/main.cpp
+++ CS6013_ComputerSystems/L3_PipesProcess/L3_PipesProcess/main.cpp
@@ -24,9 +24,10 @@ void pipe_example(string str, int len){
     
     string msg = str;
     int size = len;
+    int rc;
     
     int fds[2] = { -1, -1 };
-    int rc = pipe( fds );
+    rc = pipe( fds );
     
     int readFd = fds[0];
     int writeFd = fds[1];
@@ -48,28 +49,62 @@ void pipe_example(string str, int len){
     
     //Parent
     if( pid > 0 ){
-        close( readFd );
+        rc = close( readFd );
+        if( rc == -1){
+            perror( "Parent close() failed" );
+            exit(1);
+        }
         cout << "PARENT: my child has pid " << pid << "\n";
         
-        write(writeFd, &len, sizeof(int)); 
-        write(writeFd, &msg, size);
-    
-        wait( NULL );
+        int rc = (int)write(writeFd, &len, sizeof(int));
+        
+        if( rc == -1){
+            perror( "Parent write() size failed" );
+            exit(1);
+        }
+        rc = (int)write(writeFd, &msg, size);
+        
+        if( rc == -1){
+            perror( "Parent write() message failed" );
+            exit(1);
+        }
+        
+        rc = wait( NULL );
+        if( rc == -1){
+            perror( "Parent wait() failed" );
+            exit(1);
+        }
+        
         cout << "PARENT: done wait for child, exiting now\n";
     }
     //Child
     else {
-        close( writeFd );
+        rc = close( writeFd );
+        
+        if( rc == -1){
+            perror( "Child close() failed" );
+            exit(1);
+        }
         
         cout << "CHILD: my pid is " << getpid() << "\n";
 
         cout << "CHILD: waiting for data from parent...\n";
         
         int size;
-        read ( readFd, &size, sizeof(int) );
+        rc = (int)read ( readFd, &size, sizeof(int) );
+        
+        if( rc == -1){
+            perror( "Child read() in size failed" );
+            exit(1);
+        }
         
         char *message = (char*)malloc(size);
-        read( readFd, message, size );
+        rc = (int)read( readFd, message, size );
+        
+        if( rc == -1){
+            perror( "Child read() in message failed" );
+            exit(1);
+        }
         
         cout << "CHILD:          received: " << message <<"\n";
     }
@@ -77,9 +112,11 @@ void pipe_example(string str, int len){
 
 int main(int argc, const char * argv[]) {
     
+    errno = EFAULT;
+    
     if (argc == 2){
         const char* msg = argv[1];
-        int len = strlen(msg);
+        int len = (int)strlen(msg);
 
         pipe_example(msg, len);
     }
