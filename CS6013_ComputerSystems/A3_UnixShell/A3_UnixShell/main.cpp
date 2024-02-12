b7a97e03f80536261732de8aa5234642e338769c
Corinne Jones <jonescorinne6@gmail.com>
2024-02-12 13:56:56 -0700

    Unix Shell Changes

--- CS6013_ComputerSystems/A3_UnixShell/A3_UnixShell/main.cpp
+++ CS6013_ComputerSystems/A3_UnixShell/A3_UnixShell/main.cpp
@@ -1,109 +1,101 @@
-//
-//  main.cpp
-//  A3_UnixShell
-//
-//  Created by Corinne Jones on 2/8/24.
-//
-
 #include <iostream>
-#include "shelpers.hpp"
+#include <vector>
+#include <string>
+#include <unistd.h> // for chdir(), fork(), execvp()
+#include <sys/wait.h> // for waitpid()
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "shelpers.hpp" // Assuming this header defines tokenize() and getCommands()
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    errno = EFAULT;
-    
+int main() {
     int rc;
-    string input;
-    
-    while(true){
+    char* rawInput; //C string for readline
+
+    while (true) {
+        rawInput = readline("shell$ ");
         
-        cout << "shell$ ";
-        
-        if (!getline(cin, input)){
+        string input(rawInput); // Convert to a C++ string for easier handling
+
+        if (strlen(rawInput) > 0) {
+            add_history(rawInput); //Allows you to use the arrows to go up and down
+        }
+
+        if (input == "exit") {
+            free(rawInput); // Free input_ before exiting
             break;
         }
-        
-        if (input == "exit"){
-            break;
-        }
-        
+
         vector<string> tokens = tokenize(input);
-        vector<Command> cmds = getCommands(tokens);
         
         if (tokens[0] == "cd") {
             const char* path;
-            
+
             if (tokens.size() == 1) {
-                // 'cd' with no additional parameter should go to HOME
-                path = getenv("HOME");
+                path = getenv("HOME"); //Gets home directory from local variable
                 
-                if (path == nullptr) {
-                    perror("path did not work");
+                if(path == nullptr){
+                    perror("Path did not work");
                     continue;
                 }
             }
             else {
-                path = tokens[1].c_str();
+                path = tokens[1].c_str(); // Go to specified directory
             }
-            rc = chdir(path);
 
+            rc = chdir(path);
+            
             if(rc!=0){
                 perror("chdir() failed");
                 continue;
             }
-            continue; // Skip the rest of the loop to wait for next command
+            free(rawInput); // Free rawInput and continue to the next iteration
+            continue;
         }
-        
-        for(Command& cmd : cmds){
-            
+
+        vector<Command> cmds = getCommands(tokens);
+
+        for (Command& cmd : cmds) {
             int pid = fork();
-            
-            if ( pid < 0 ){
-                perror( "fork() failed" );
+
+            if (pid < 0) {
+                perror("fork() failed");
                 exit(1);
             }
-            
-            //parent
-            else if( pid > 0 ){
-                
-                if(cmd.inputFd != STDIN_FILENO){
-                    close(cmd.inputFd); 
+            else if (pid > 0) {
+                // Parent process
+                if (cmd.inputFd != STDIN_FILENO) {
+                    close(cmd.inputFd);
                 }
-                
-                if(cmd.outputFd != STDOUT_FILENO){
+
+                if (cmd.outputFd != STDOUT_FILENO) {
                     close(cmd.outputFd);
                 }
-                
+
                 int status;
-                rc = waitpid(pid, &status, 0);
-                
-                if (rc == -1) {
-                    perror( "waitpid() failed" );
-                    exit(1);
-                }
+                waitpid(pid, &status, 0); // Wait for the child process to finish
             }
-            
-            //child
-            else{
-                
-                if(cmd.inputFd != STDIN_FILENO){
+            else {
+                // Child process
+                if (cmd.inputFd != STDIN_FILENO) {
                     dup2(cmd.inputFd, STDIN_FILENO);
                 }
-                
-                if(cmd.outputFd != STDOUT_FILENO){
+
+                if (cmd.outputFd != STDOUT_FILENO) {
                     dup2(cmd.outputFd, STDOUT_FILENO);
                 }
-                
-                rc = execvp(cmd.execName.c_str(), const_cast<char* const*>(cmd.argv.data()));
-                
-                if (rc == -1) {
-                    perror( "execvp() failed" );
-                    exit(1);
-                }
+
+                execvp(cmd.execName.c_str(), const_cast<char* const*>(cmd.argv.data()));
+
+                // If execvp returns, it must have failed
+                perror("execvp() failed");
+                exit(1);
             }
         }
+
+        free(rawInput); // Ensure rawInput is freed at the end of each loop iteration
     }
+
+    return 0;
 }
