2f2d172624442850aec98ab5449df60571cb8781
Corinne Jones <jonescorinne6@gmail.com>
2024-02-22 13:58:48 -0700

    Update test_msdscript.cpp

--- CS6015_SoftwareEngineering/MSDScript/MSDScript/test_msdscript.cpp
+++ CS6015_SoftwareEngineering/MSDScript/MSDScript/test_msdscript.cpp
@@ -5,7 +5,7 @@ using namespace std;
 static string random_expr_string();
 static string random_variable();
 void testSingleImpl(string path);
-void testDualImpl(string path1, path2);
+void testDualImpl(string path1, string path2);
 
 int main(int argc, char **argv) {
     
@@ -19,12 +19,12 @@ int main(int argc, char **argv) {
         testDualImpl(path1, path2);
     }
     else{
-        
+        cout<< "Usage: test_msdscript <msdscript_path> OR test_msdscript <msdscript_path_1> <msdscript_path_2>";
+        exit(1);
     }
     
-    
-    
     srand (clock());
+    
     for (int i = 0; i < 10; i++) {
         string s = "";
         s=(rand() % 26) + 'a';
@@ -35,14 +35,23 @@ int main(int argc, char **argv) {
 
 return 0;
 }
+void(){
+    
+}
 
-
+//function test_single_implementation(path_to_msdscript):
+//    for each test_case in test_cases:
+//        expression = generate_random_expression()
+//        result = run_msdscript(path_to_msdscript, expression, "--interp")
+//        if result is not as expected:
+//            report_failure(expression, result)
+//    print("Single implementation testing completed.")
 
 string random_expr_string() {
     if ((rand() % 10) < 6){
         return to_string(rand());
     }
-    else{
+    else {
         return random_expr_string() + "+" + random_expr_string();
     }
 }
