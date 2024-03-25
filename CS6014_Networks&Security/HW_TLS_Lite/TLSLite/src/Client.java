2be930256ed73eaa9ed663c9856f5f709900b321
Corinne Jones <jonescorinne6@gmail.com>
2024-03-25 12:50:27 -0600

    Hmac verification not working, committing prior to refactoring

--- CS6014_Networks&Security/HW_TLS_Lite/TLSLite/src/Client.java
+++ CS6014_Networks&Security/HW_TLS_Lite/TLSLite/src/Client.java
@@ -16,14 +16,6 @@ public class Client {
     final static String HOST = "localhost";
     final static int PORT_NUM = 8080;
 
-    public byte[] generateNonce(){
-        SecureRandom random = new SecureRandom();
-        byte[] nonce = new byte[32];
-        random.nextBytes(nonce);
-
-        return nonce;
-    }
-
     public void addMessage(final byte[] message){
         messageHistory.add(message);
     }
@@ -46,7 +38,26 @@ public class Client {
         return masterKey;
     }
 
-    public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, ClassNotFoundException, CertificateException, SignatureException, InvalidKeyException, NoSuchProviderException {
+    private ArrayList<byte[]> getMessageHistory(){
+        return messageHistory;
+    }
+
+    /**
+     * Generates a cryptographically strong random nonce.
+     *
+     * This method creates a 32-byte (256-bit) nonce using a secure random number generator. A nonce is a "number used once"
+     * and is critical in cryptographic protocols to ensure that communications are unique and to prevent replay attacks.
+     *
+     * @return A 32-byte array containing the generated nonce.
+     */
+    public byte[] generateNonce(){
+        SecureRandom random = new SecureRandom();
+        byte[] nonce = new byte[32];
+        random.nextBytes(nonce); //Fill the byte array with random data
+
+        return nonce;
+    }
+    public static void main(String[] args) throws Exception {
 
         //establish a connection with the server
         Socket socket = new Socket(HOST, PORT_NUM);
@@ -68,36 +79,57 @@ public class Client {
 
         ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
 
-        //MESSAGE 2: Receive a.) certificate, b.) DHPublicKey c.) signed DHPublicKey
+        //MESSAGE 2: Receive a) certificate, b) DHPublicKey c) signed DHPublicKey
         Certificate serverCertificate = (Certificate)is.readObject();
         client.addMessage(serverCertificate.toString().getBytes());
         BigInteger serverDHPublicKey = (BigInteger)is.readObject();
         client.addMessage(serverDHPublicKey.toByteArray());
-        BigInteger serverSignedDHKey = (BigInteger)is.readObject();
-        client.addMessage(serverSignedDHKey.toByteArray());
+        byte[] serverSignedDHKey = (byte[]) is.readObject();
+        //need to decrypt the signed DH Key
+        client.addMessage(serverSignedDHKey);
         System.out.println("Client received message 2" + "\n");
 
 
         //Message 3: Send a) certificate, b) DHPublicKey, c) signed DHPublicKey
         Certificate clientCertificate = Shared.getHostCertificate("client");
-        PublicKey clientRSAPublicKey = clientCertificate.getPublicKey();
-        BigInteger clientSignedDHKey = Shared.generateSignedDHKey("client", client.getClientDHPublicKey(), clientRSAPublicKey);
+//        PublicKey clientRSAPublicKey = clientCertificate.getPublicKey();
+        byte[] clientSignedDHKey_Byte = Shared.generateSignedDHKey("client", client.getClientDHPublicKey());
 
         os.writeObject(clientCertificate);
         client.addMessage(clientCertificate.toString().getBytes());
         os.writeObject(client.getClientDHPublicKey());
         client.addMessage(client.getClientDHPublicKey().toByteArray());
-        os.writeObject(clientSignedDHKey);
-        client.addMessage(clientSignedDHKey.toByteArray());
+        os.writeObject(clientSignedDHKey_Byte);
+        client.addMessage(clientSignedDHKey_Byte);
         System.out.println("Client sent message 3");
 
         if(Shared.validateCertificate(serverCertificate)){
             client.initializeMasterKey(Shared.generateMasterKey(client.getClientDHPublicKey(), client.getClientDHPrivateKey()));
-            Shared.generateSessionKeys(client.getMasterKey());
+            Shared.generateSessionKeys(nonce, client.getMasterKey().toByteArray());
         }
         else{
-            //this means it did not validate
-            //todo: figure out what to do if not valid
+            //todo: figure out what to do if does not validate
+            socket.close();
+        }
+
+        //Message 4: Receive HMAC msg of history so far
+        byte[] serverHMACMessage = (byte[])is.readObject();
+        client.addMessage(serverHMACMessage);
+        System.out.println("Client received message 4");
+
+
+        //Message 5: Send HMAC msg of history so far
+        byte[] clientHMACMessage = Shared.generateMacMessage(client.getMessageHistory(), Shared.clientMAC);
+        os.writeObject(clientHMACMessage);
+        client.addMessage(clientHMACMessage);
+        System.out.println("Client sent message 5");
+
+        //VERIFY VALID HMAC
+        if(Shared.verifyMessageHistory(serverHMACMessage, client.getMessageHistory(), Shared.clientMAC)){
+            System.out.println("CLIENT HANDSHAKE COMPLETED YESSSS QUEEN");
+        }
+        else {
+            System.out.println("NOT THE SAME DUMMY");
         }
 
         socket.close();
--- CS6014_Networks&Security/HW_TLS_Lite/TLSLite/src/Server.java
+++ CS6014_Networks&Security/HW_TLS_Lite/TLSLite/src/Server.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.math.BigInteger;
 import java.net.Socket;
-import java.security.*;
-import java.security.cert.CertificateException;
 import java.security.cert.Certificate;
 import java.net.ServerSocket;
 import java.util.ArrayList;
@@ -34,8 +32,11 @@ public class Server {
     private BigInteger getMasterKey(){
         return masterKey;
     }
+    private ArrayList<byte[]> getMessageHistory(){
+        return messageHistory;
+    }
 
-    public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, ClassNotFoundException, CertificateException, SignatureException, InvalidKeyException, NoSuchProviderException {
+    public static void main(String[] args) throws Exception {
 
         Server server = new Server();
         ServerSocket ss = new ServerSocket(PORT_NUM);
@@ -60,34 +61,57 @@ public class Server {
 
         //MESSAGE 2: Send a) certificate, b) DHPublicKey, c) signed DHPublicKey
         Certificate serverCertificate = Shared.getHostCertificate("server");
-        PublicKey serverRSAPublicKey = serverCertificate.getPublicKey();
-        BigInteger serverSignedDHKey = Shared.generateSignedDHKey("server", server.getServerDHPublicKey(), serverRSAPublicKey);
+//        PublicKey serverRSAPublicKey = serverCertificate.getPublicKey();
+        byte[] serverSignedDHKey_Bytes = Shared.generateSignedDHKey("server", server.getServerDHPublicKey());
 
         os.writeObject(serverCertificate);
         server.addMessage(serverCertificate.toString().getBytes());
         os.writeObject(server.getServerDHPublicKey());
         server.addMessage(server.getServerDHPublicKey().toByteArray());
-        os.writeObject(serverSignedDHKey);
-        server.addMessage(serverSignedDHKey.toByteArray());
+        os.writeObject(serverSignedDHKey_Bytes);
+        server.addMessage(serverSignedDHKey_Bytes);
         System.out.println("Server sent message 2\n");
 
-        //MESSAGE 3: Receive a.) certificate, b.) DHPublicKey c.) signed DHPublicKey
+        //MESSAGE 3: Receive a) certificate, b) DHPublicKey c) signed DHPublicKey
         Certificate clientCertificate = (Certificate)is.readObject();
         server.addMessage(clientCertificate.toString().getBytes());
         BigInteger clientDHPublicKey = (BigInteger)is.readObject();
         server.addMessage(clientDHPublicKey.toByteArray());
-        BigInteger clientSignedDHKey = (BigInteger)is.readObject();
-        server.addMessage(clientSignedDHKey.toByteArray());
+        byte[] clientSignedDHKey = (byte[])is.readObject();
+        //need to encrypt the signed key prior to sending
+        server.addMessage(clientSignedDHKey);
         System.out.println("Server received message 3");
 
         if(Shared.validateCertificate(clientCertificate)){
             server.initializeMasterKey(Shared.generateMasterKey(server.getServerDHPublicKey(), server.getServerDHPrivateKey()));
-            Shared.generateSessionKeys(server.getMasterKey());
+            Shared.generateSessionKeys(nonce, server.getMasterKey().toByteArray());
         }
         else{
-            //this means it did not validate
-            //todo: figure out what to do if not valid
+            //todo: figure out what to do if does not validate
+            is.close();
+            socket.close();
+            ss.close();
         }
+
+        //MESSAGE 4: Send HMAC msg of history so far
+        byte[] serverHMACMessage = Shared.generateMacMessage(server.getMessageHistory(), Shared.serverMAC);
+        os.writeObject(serverHMACMessage);
+        server.addMessage(serverHMACMessage);
+        System.out.println("Server sent message 4");
+
+        //Message 5: Receive HMAC msg of history so far
+        byte[] clientHMACMessage = (byte[])is.readObject();
+        server.addMessage(clientHMACMessage);
+        System.out.println("Server received message 5");
+
+        //VERIFY VALID HMAC
+        if(Shared.verifyMessageHistory(clientHMACMessage, server.getMessageHistory(), Shared.serverMAC)){
+            System.out.println("SERVER HANDSHAKE COMPLETED YESSSS QUEEN");
+        }
+        else {
+            System.out.println("NOT THE SAME DUMMY");
+        }
+
 
         is.close();
         socket.close();
